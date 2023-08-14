31d02625413db4d7801c17c1492d00ee716cb93c
Zatt010 <afuheho1@gmail.com>
2023-08-13 23:13:32 -0400

    Cuarta funcionalidad, genero, nombr,edad,hora e idioma1.1

--- dist/index.87ae31f1.js
+++ dist/index.87ae31f1.js
@@ -578,17 +578,24 @@ var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _saludadorJs = require("./saludador.js");
 var _saludadorJsDefault = parcelHelpers.interopDefault(_saludadorJs);
 const botForm = document.getElementById("bot-form");
+const idiomaDiv = document.getElementById("idioma-div");
 const saludoDiv = document.getElementById("saludo-div");
 const generoDiv = document.getElementById("genero-div");
 const edadDiv = document.getElementById("edad-div");
 const generoButton = document.getElementById("genero-button");
 const edadButton = document.getElementById("edad-button");
+const idiomaButton = document.getElementById("idioma-button");
+const idiomaSelect = document.getElementById("idioma");
 let genero = "";
 botForm.addEventListener("submit", (event)=>{
     event.preventDefault();
     const nameInput = document.getElementById("name");
     const saludo = (0, _saludadorJsDefault.default)(nameInput);
     saludoDiv.innerHTML = saludo;
+    idiomaDiv.style.display = "block";
+});
+idiomaButton.addEventListener("click", ()=>{
+    idiomaDiv.style.display = "none";
     generoDiv.style.display = "block";
 });
 generoButton.addEventListener("click", ()=>{
@@ -615,10 +622,19 @@ edadButton.addEventListener("click", ()=>{
     if (horaActual >= 0 && horaActual < 12) saludoHora = "Buenos d\xedas";
     else if (horaActual >= 12 && horaActual < 18) saludoHora = "Buenas tardes";
     else saludoHora = "Buenas noches";
+    const selectedIdioma = idiomaSelect.value;
+    if (selectedIdioma === "es") mostrarSaludoCompleto(saludoHora, generoSaludo, name);
+    else if (selectedIdioma === "en") mostrarSaludoIngles();
+});
+function mostrarSaludoCompleto(saludoHora, generoSaludo, name) {
     const mensajeFinal = `${saludoHora} y ${generoSaludo} ${name}`;
     saludoDiv.innerHTML = mensajeFinal;
     edadDiv.style.display = "none";
-});
+}
+function mostrarSaludoIngles() {
+    saludoDiv.innerHTML = "Hello"; // Muestra "Hello" en lugar del saludo completo
+    edadDiv.style.display = "none";
+}
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./saludador.js":"3Yd5J"}],"gkKU3":[function(require,module,exports) {
 exports.interopDefault = function(a) {
--- src/presenter.js
+++ src/presenter.js
@@ -1,10 +1,13 @@
 import saludar from "./saludador.js";
 const botForm = document.getElementById("bot-form");
+const idiomaDiv = document.getElementById("idioma-div");
 const saludoDiv = document.getElementById("saludo-div");
 const generoDiv = document.getElementById("genero-div");
 const edadDiv = document.getElementById("edad-div");  
 const generoButton = document.getElementById("genero-button");
 const edadButton = document.getElementById("edad-button");  
+const idiomaButton = document.getElementById("idioma-button");
+const idiomaSelect = document.getElementById("idioma");
 let genero = "";
 
 botForm.addEventListener("submit", (event) => {
@@ -12,6 +15,11 @@ botForm.addEventListener("submit", (event) => {
   const nameInput = document.getElementById("name");
   const saludo = saludar(nameInput);
   saludoDiv.innerHTML = saludo;
+  idiomaDiv.style.display = "block";
+});
+
+idiomaButton.addEventListener("click", () => {
+  idiomaDiv.style.display = "none";
   generoDiv.style.display = "block";
 });
 
@@ -56,7 +64,22 @@ edadButton.addEventListener("click", () => {
     saludoHora = "Buenas noches";
   }
 
+  const selectedIdioma = idiomaSelect.value;
+
+  if (selectedIdioma === "es") {
+    mostrarSaludoCompleto(saludoHora, generoSaludo, name);
+  } else if (selectedIdioma === "en") {
+    mostrarSaludoIngles();
+  }
+});
+
+function mostrarSaludoCompleto(saludoHora, generoSaludo, name) {
   const mensajeFinal = `${saludoHora} y ${generoSaludo} ${name}`;
   saludoDiv.innerHTML = mensajeFinal;
   edadDiv.style.display = "none";
-});
+}
+
+function mostrarSaludoIngles() {
+  saludoDiv.innerHTML = "Hello"; // Muestra "Hello" en lugar del saludo completo
+  edadDiv.style.display = "none";
+}
